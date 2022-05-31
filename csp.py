0192dda229d25636eb359e9a088692101e780597
Nader Essam <73783520+naderessam10@users.noreply.github.com>
2022-05-31 02:58:12 +0200

    Update csp.py

--- csp.py
+++ csp.py
@@ -80,28 +80,11 @@ class CSP(search.Problem):
                     not self.constraints(var, val, var2, assignment[var2]))
         return count(conflict(v) for v in self.neighbors[var])
 
-    def display(self, assignment):
-        """Show a human-readable representation of the CSP."""
-        # Subclasses can print in a prettier way, or display with a GUI
-        print('CSP:', self, 'with assignment:', assignment)
+
 
     # These methods are for the tree and graph-search interface:
 
-    def actions(self, state):
-        """Return a list of applicable actions: nonconflicting
-        assignments to an unassigned variable."""
-        if len(state) == len(self.variables):
-            return []
-        else:
-            assignment = dict(state)
-            var = first([v for v in self.variables if v not in assignment])
-            return [(var, val) for val in self.domains[var]
-                    if self.nconflicts(var, val, assignment) == 0]
 
-    def result(self, state, action):
-        """Perform an action and return the new state."""
-        (var, val) = action
-        return state + ((var, val),)
 
     def goal_test(self, state):
         """The goal is to assign all variables, with all constraints satisfied."""
@@ -146,12 +129,9 @@ class CSP(search.Problem):
         for B, b in removals:
             self.curr_domains[B].append(b)
 
-    # This is for min_conflicts search
 
-    def conflicted_vars(self, current):
-        """Return a list of variables in current assignment that are in conflict"""
-        return [var for var in self.variables
-                if self.nconflicts(var, current[var], current) > 0]
+
+
 
 # ______________________________________________________________________________
 # Constraint Propagation with AC-3
@@ -183,30 +163,15 @@ def revise(csp, Xi, Xj, removals):
             revised = True
     return revised
 
+
 # ______________________________________________________________________________
 # CSP Backtracking Search
 
 # Variable ordering
 
-
 def first_unassigned_variable(assignment, csp):
     """The default variable order."""
     return first([var for var in csp.variables if var not in assignment])
-
-
-def mrv(assignment, csp):
-    """Minimum-remaining-values heuristic."""
-    return argmin_random_tie(
-        [v for v in csp.variables if v not in assignment],
-        key=lambda var: num_legal_values(csp, var, assignment))
-
-
-def num_legal_values(csp, var, assignment):
-    if csp.curr_domains:
-        return len(csp.curr_domains[var])
-    else:
-        return count(csp.nconflicts(var, val, assignment) == 0
-                     for val in csp.domains[var])
 
 # Value ordering
 
@@ -214,12 +179,6 @@ def num_legal_values(csp, var, assignment):
 def unordered_domain_values(var, assignment, csp):
     """The default value order."""
     return csp.choices(var)
-
-
-def lcv(var, assignment, csp):
-    """Least-constraining-values heuristic."""
-    return sorted(csp.choices(var),
-                  key=lambda val: csp.nconflicts(var, val, assignment))
 
 # Inference
 
@@ -274,35 +233,7 @@ def backtracking_search(csp,
     assert result is None or csp.goal_test(result)
     return result
 
-# ______________________________________________________________________________
-# Min-conflicts hillclimbing search for CSPs
 
-
-def min_conflicts(csp, max_steps=100000):
-    """Solve a CSP by stochastic hillclimbing on the number of conflicts."""
-    # Generate a complete assignment for all variables (probably with conflicts)
-    csp.current = current = {}
-    for var in csp.variables:
-        val = min_conflicts_value(csp, var, current)
-        csp.assign(var, val, current)
-    # Now repeatedly choose a random conflicted variable and change it
-    for i in range(max_steps):
-        conflicted = csp.conflicted_vars(current)
-        if not conflicted:
-            return current
-        var = random.choice(conflicted)
-        val = min_conflicts_value(csp, var, current)
-        csp.assign(var, val, current)
-    return None
-
-
-def min_conflicts_value(csp, var, current):
-    """Return the value that will give var the least number of conflicts.
-    If there is a tie, choose at random."""
-    return argmin_random_tie(csp.domains[var],
-                             key=lambda val: csp.nconflicts(var, val, current))
-
-# ______________________________________________________________________________
 
 
 def tree_csp_solver(csp):
@@ -392,79 +323,4 @@ def assign_value(Xj, Xk, csp, assignment):
 
     # No consistent assignment available
     return None
-
-# ______________________________________________________________________________
-# Map-Coloring Problems
-
-
-class UniversalDict:
-    """A universal dict maps any key to the same value. We use it here
-    as the domains dict for CSPs in which all variables have the same domain.
-    >>> d = UniversalDict(42)
-    >>> d['life']
-    42
-    """
-
-    def __init__(self, value): self.value = value
-
-    def __getitem__(self, key): return self.value
-
-    def __repr__(self): return '{{Any: {0!r}}}'.format(self.value)
-
-
-def different_values_constraint(A, a, B, b):
-    """A constraint saying two neighboring variables must differ in value."""
-    return a != b
-
-
-def MapColoringCSP(colors, neighbors):
-    """Make a CSP for the problem of coloring a map with different colors
-    for any two adjacent regions. Arguments are a list of colors, and a
-    dict of {region: [neighbor,...]} entries. This dict may also be
-    specified as a string of the form defined by parse_neighbors."""
-    if isinstance(neighbors, str):
-        neighbors = parse_neighbors(neighbors)
-    return CSP(list(neighbors.keys()), UniversalDict(colors), neighbors,
-               different_values_constraint)
-
-
-def parse_neighbors(neighbors, variables=None):
-    """Convert a string of the form 'X: Y Z; Y: Z' into a dict mapping
-    regions to neighbors. The syntax is a region name followed by a ':'
-    followed by zero or more region names, followed by ';', repeated for
-    each region name. If you say 'X: Y' you don't need 'Y: X'.
-    >>> parse_neighbors('X: Y Z; Y: Z') == {'Y': ['X', 'Z'], 'X': ['Y', 'Z'], 'Z': ['X', 'Y']}
-    True
-    """
-    dic = defaultdict(list)
-    specs = [spec.split(':') for spec in neighbors.split(';')]
-    for (A, Aneighbors) in specs:
-        A = A.strip()
-        for B in Aneighbors.split():
-            dic[A].append(B)
-            dic[B].append(A)
-    return dic
-
-
-australia = MapColoringCSP(list('RGB'),
-                           'SA: WA NT Q NSW V; NT: WA Q; NSW: Q V; T: ')
-
-usa = MapColoringCSP(list('RGBY'),
-                     """WA: OR ID; OR: ID NV CA; CA: NV AZ; NV: ID UT AZ; ID: MT WY UT;
-        UT: WY CO AZ; MT: ND SD WY; WY: SD NE CO; CO: NE KA OK NM; NM: OK TX AZ;
-        ND: MN SD; SD: MN IA NE; NE: IA MO KA; KA: MO OK; OK: MO AR TX;
-        TX: AR LA; MN: WI IA; IA: WI IL MO; MO: IL KY TN AR; AR: MS TN LA;
-        LA: MS; WI: MI IL; IL: IN KY; IN: OH KY; MS: TN AL; AL: TN GA FL;
-        MI: OH IN; OH: PA WV KY; KY: WV VA TN; TN: VA NC GA; GA: NC SC FL;
-        PA: NY NJ DE MD WV; WV: MD VA; VA: MD DC NC; NC: SC; NY: VT MA CT NJ;
-        NJ: DE; DE: MD; MD: DC; VT: NH MA; MA: NH RI CT; CT: RI; ME: NH;
-        HI: ; AK: """)
-
-france = MapColoringCSP(list('RGBY'),
-                        """AL: LO FC; AQ: MP LI PC; AU: LI CE BO RA LR MP; BO: CE IF CA FC RA
-        AU; BR: NB PL; CA: IF PI LO FC BO; CE: PL NB NH IF BO AU LI PC; FC: BO
-        CA LO AL RA; IF: NH PI CA BO CE; LI: PC CE AU MP AQ; LO: CA AL FC; LR:
-        MP AU RA PA; MP: AQ LI AU LR; NB: NH CE PL BR; NH: PI IF CE NB; NO:
-        PI; PA: LR RA; PC: PL CE LI AQ; PI: NH NO CA IF; PL: BR NB CE PC; RA:
-        AU BO FC PA LR""")
 
